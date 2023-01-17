451b3098b840c2e35e0feabd19daac1e6de1e00c
Rolo <loureiroraul@gmail.com>
2023-01-17 12:19:52 -0300

    initial page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  content: ["./**/*.html"],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      colors: {},
-    },
-  },
-  variants: {},
-  plugins: [require("@tailwindcss/typography")],
+	content: ['./**/*.html'],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {},
+			fontFamily: {
+				raleway: ['Raleway'],
+				inter: ['Inter'],
+			},
+		},
+	},
+	variants: {},
+	plugins: [require('@tailwindcss/typography')],
 };
